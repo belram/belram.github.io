66b854679c499522ad174fd01b9d49efaee751ea
belram <30906145+belram@users.noreply.github.com>
2017-08-24 23:19:48 +0300

    Create lesson5.php

---
+++ lesson5.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PhoneBook</title>
+</head>
+<body>
+
+<p>
+<?php
+$jsonString = file_get_contents('country.json');
+$data = json_decode($jsonString, true);
+?>
+<table>
+<tr>
+<td>FirstName</td>
+<td>LastName</td>
+<td>Address</td>
+<td>PhoneNumber</td>
+</tr>
+<?php foreach ($data as $datum) {?>
+<tr>
+<td><?php echo $datum['firstName'] ?></td>
+<td><?php echo $datum['lastName'] ?></td>
+<td><?php echo $datum['address'] ?></td>
+<td><?php echo $datum['phoneNumber'] ?></td>
+</tr>
+<?php } ?>
+</table>
+
+[
+	{"firstName": "Иван", "lastName": "Иванов", "address": "г.Москва, ул. Алиева,2", "phoneNumber": "812 123-1234"},
+	{"firstName": "Максим", "lastName": "Петров", "address": "г.Воронеж, ул. Есенина,7", "phoneNumber": "844 555-1234"},
+	{"firstName": "Юрий", "lastName": "Сидоров", "address": "г.Казань, ул. Пушкина,3", "phoneNumber": "833 666-7654"},
+	{"firstName": "Bill", "lastName": "Сидоров", "address": "г.Казань, ул. Пушкина,3", "phoneNumber": "833 666-7654"},
+	{"firstName": "Jake", "lastName": "Сидоров", "address": "г.Саратов, ул. Пушкина,3", "phoneNumber": "833 666-7654"},
+	{"firstName": "Татьяна", "lastName": "Сидоров", "address": "г.Казань, ул. Пушкина,3", "phoneNumber": "833 666-7654"},
+	{"firstName": "Михаил", "lastName": "Сидоров", "address": "г.Казань, ул. Пушкина,3", "phoneNumber": "833 666-7654"},
+	{"firstName": "Виктор", "lastName": "Сидоров", "address": "г.Казань, ул. Пушкина,3", "phoneNumber": "833 666-7654"}
+]
