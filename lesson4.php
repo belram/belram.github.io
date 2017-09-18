63daf7b108c875979024aa4794903c6b3296001a
belram <30906145+belram@users.noreply.github.com>
2017-09-19 01:40:13 +0300

    Update lesson4.php

--- lesson4.php
+++ lesson4.php
@@ -1,8 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Погода в Москве</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
 <?php 
 
-$content = file_get_contents('http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1');
+$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&units=metric&APPID=a13020b8efb43f553275a23adb4440cb');
 
-echo '<pre>';
-print_r(json_decode($content, true));
-echo '</pre>';
+file_put_contents('weather.json', $content);
+
+$data = json_decode($content, true);
+
+// print_r($data);
 ?>
+
+<style>
+	td{
+		height: 20px;
+		width: 100px;
+		border: 1px solid black;
+		text-align: center;
+	}
+</style>
+
+<h1>Погода в Москве</h1>
+<table>
+	<tr>
+		<td>Текущая температура</td>
+		<td>Скорость ветра</td>
+		<td>Влажность</td>
+		<td>Давление</td>
+	</tr>
+	<tr>
+		<td><?php print $data['main']['temp']; ?></td>
+		<td><?php print $data['wind']['speed']; ?></td>
+		<td><?php print $data['main']['humidity']; ?></td>
+		<td><?php print $data['main']['pressure']; ?></td>
+	</tr>
+</table>
+
+</body>
+</html>
