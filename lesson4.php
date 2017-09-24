abff40cb74a9b23f3d97a3594acf1b959ce4845a
belram <30906145+belram@users.noreply.github.com>
2017-09-25 00:18:16 +0300

    Update lesson4.php

--- lesson4.php
+++ lesson4.php
@@ -1,46 +1,45 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Погода в Москве</title>
-	<meta charset="utf-8">
-</head>
-<body>
-
 <?php 
 
-$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&units=metric&APPID=a13020b8efb43f553275a23adb4440cb');
+$next_data = [];
+function get_weather($filename){
+	$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&units=metric&APPID=a13020b8efb43f553275a23adb4440cb');
+	file_put_contents($filename, $content);
+	$data = json_decode($content, true);
+	global $next_data;
+	$next_data = $data;
+}
 
-file_put_contents('weather.json', $content);
+function have_weather($filename){
+	$jsonString = file_get_contents($filename);
+	$data = json_decode($jsonString, true);
+	global $next_data;
+	$next_data = $data;
+}
 
-$data = json_decode($content, true);
+function show_weather(){
+	global $next_data;
+	print '<!DOCTYPE html><html><head><title>Погода в Москве</title><meta charset="utf-8"></head><body><style>td{height: 20px;width: 100px;border: 1px solid black;text-align: center;}</style><h1>Погода в Москве</h1><table><tr><td>Текущая температура</td><td>Скорость ветра</td><td>Влажность</td><td>Давление</td></tr><tr>';
+	print "<td>{$next_data['main']['temp']}</td>";
+	print "<td>{$next_data['wind']['speed']}</td>";
+	print "<td>{$next_data['main']['humidity']}</td>";
+	print "<td>{$next_data['main']['pressure']}</td>";
+	print '</tr></table></body></html>';
+}
 
-// print_r($data);
-?>
+$filename = 'cache.txt';
+$time = time() - 3600;
 
-<style>
-	td{
-		height: 20px;
-		width: 100px;
-		border: 1px solid black;
-		text-align: center;
+if (file_exists($filename)){
+	if(!touch($filename, $time)){
+		get_weather($filename);
+		show_weather();
+	}else{
+		have_weather($filename);
+		show_weather();
 	}
-</style>
+}else{
+	get_weather($filename);
+	show_weather();
+}
 
-<h1>Погода в Москве</h1>
-<table>
-	<tr>
-		<td>Текущая температура</td>
-		<td>Скорость ветра</td>
-		<td>Влажность</td>
-		<td>Давление</td>
-	</tr>
-	<tr>
-		<td><?php print $data['main']['temp']; ?></td>
-		<td><?php print $data['wind']['speed']; ?></td>
-		<td><?php print $data['main']['humidity']; ?></td>
-		<td><?php print $data['main']['pressure']; ?></td>
-	</tr>
-</table>
-
-</body>
-</html>
+?>
