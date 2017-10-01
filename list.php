46d37c342c0d982f421d8eb02dc84dd27def2fdb
belram <30906145+belram@users.noreply.github.com>
2017-10-01 20:46:38 +0300

    Update list.php

--- list.php
+++ list.php
@@ -7,22 +7,6 @@
 <body>
 
 <style>
-	.sours {
-		display: inline-block;
-		height: 20px;
-		width: 150px;
-		margin-bottom: 5px;
-		
-	}
-	.statement{
-		
-		padding: 10px 15px;
-		height: 30px;
-		min-width: 150px;
-		max-width: 300px;
-		font-size: 15px;
-		border: 1px solid black;
-	}
 	table{
 		border-collapse: collapse;
 	}
@@ -33,38 +17,13 @@
 	}
 </style>
 
-<h1>Выберете тест</h1>
-	<div class="sours">
-		Состояние загрузки:
-	</div>
 
-	<div class="statement">
-	<?php
-
-if(substr(($_FILES['myfile']['name']), -4) == 'json'){
-	if(isset($_FILES['myfile']) && !empty($_FILES['myfile']['name'])){
-	 	if($_FILES['myfile']['error'] == UPLOAD_ERR_OK &&
-	 		move_uploaded_file($_FILES['myfile']['tmp_name'], "tests/" . $_FILES['myfile']['name']))
-	 	{
-	 		echo "Файл загружен: " . $_FILES['myfile']['name'] . "\n";
-	 		
-	 	}
-	 	else {
-	 		echo "Ошибка: повторите попытку";
-	 	}
-	 }
-}else{
-	echo "Недопустимый формат файла";
-}
-
-	?>
-	</div>
 <h4>Список тестов</h4>
 
 <?php
-
 $dir = 'tests/';
 $loaded_files = scandir($dir);
+
 ?>
 <table>
 	<tr>
