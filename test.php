04b07ee76133a936df85c95ae05b6ee4478bfd39
belram <30906145+belram@users.noreply.github.com>
2017-10-01 20:48:01 +0300

    Update test.php

--- test.php
+++ test.php
@@ -16,13 +16,21 @@
 
 <?php
 
+print_r($_GET);
+
 $next_data = [];
 foreach ($_GET as $key => $value) {
-	$file_name = "tests/" . $value;
-	$jsonString = file_get_contents("$file_name");
-	$data = json_decode($jsonString, true);
-	global $next_data;
-	$next_data = $data;
+	$clear_key = htmlspecialchars(("$key"));
+	$clear_value = htmlspecialchars(("$value"));
+	if((substr($clear_value, -4)) == 'json'){
+		$file_name = "tests/" . $clear_value;
+		$jsonString = file_get_contents("$file_name");
+		$data = json_decode($jsonString, true);
+		global $next_data;
+		$next_data = $data;
+	}else{
+		print 'Недопустимый формат файла';
+	}
 }
 
 $correct = [];
@@ -34,18 +42,22 @@ foreach($data as $num => $val){
 	foreach($val as $num_2 => $val_2){
 		if((strpos($num_2, 'question')) !== false){
 			print '<fieldset>' . "<br>\n";
-			print '<legend>' . "$val[$num_2]" . '</legend>' . "<br>\n";
+			print '<legend>' . htmlspecialchars(("$val[$num_2]")) . '</legend>' . "<br>\n";
 		}
 		global $correct;
 		if($num_2 == 'correct'){
-			$correct[] = "$val[$num_2]";
+			$cor = "$val[$num_2]";
+			$corStr = (string)$cor;
+			$correct[] = htmlspecialchars(("$corStr"));
 		}
 		if((is_array($val_2)) !== false){
 			foreach($val_2 as $num_3 => $val_3){
 				global $i;
-				print '<input type="radio" name="q' . "$i" . '" value="' . "$num_3" .  '">' . "$val_3" . "<br>\n";
+				$varStr = $val_3;
+				$i_varStr = (int)$varStr;
+				print '<input type="radio" name="q' . "$i" . '" value="' . htmlspecialchars(("$num_3")) .  '">' . htmlspecialchars(("$i_varStr")) . "<br>\n";
 			}
-			print '</fieldset>' . "<br>\n";			
+			print '</fieldset>' . "<br>\n";		
 		}
 	}
 	$i++;
@@ -56,29 +68,20 @@ print "<br>";
 print '<input class="check" type="submit" value="Check">' . "<br>\n";
 print '</form>';
 
+if(isset($_POST)){
+	$result = [];
+	foreach($_POST as $f_key => $f_vel){
+		$result[] = filter_input(INPUT_POST, "$f_key", FILTER_SANITIZE_SPECIAL_CHARS);
+	}
+}
 
-$result = array_values($_POST);
-
-if(count($result)){
-	fun_first();
-	for($m = 0, $w = 1; $m < count($result); $m++, $w++){
+print 'Вы правильно ответиль на вопросы: ';
+for($m = 0, $w = 1; $m < count($result); $m++, $w++){
 		if($correct[$m] === $result[$m]){
 			print "$w ";
 		}
 	}
-	fun_last();
-}
-
-function fun_first(){
-	print '<style>.parent {width: 100%;height: 100%;position: absolute;top: 0;left: 0;overflow: auto;background-color: #6f0573;opacity: .92;}.block{width: 360px;height: 150px;position: absolute;top: 0;right: 0;bottom: 0;left: 0;margin: auto;background-color: blue;opacity: 1;text-align: center;}.ans {display: inline-block;margin-top: 50px;color: #ffffff;}.back{position: absolute;bottom: 30px;left: 140px;text-align: center;}</style>';
-	print '<div class="parent"><div class="block"><p class="ans">Вы правильно ответили на вопрос(ы) № ';
-}
-
-function fun_last(){
-	print '</p><form class="back" action="list.php" method="post"><input type="submit" name="back" value="List of tests"></form></div></div>';
-}
 
 ?>
-
 </body>
 </html>
