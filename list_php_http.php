a2f984288730fca3c1a9673f2a0cb0f36972d76e
belram <30906145+belram@users.noreply.github.com>
2017-10-03 12:08:25 +0300

    Create list_php_http.php

---
+++ list_php_http.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Список загруженных тестов</title>
+</head>
+<body>
+
+<style>
+	table{
+		border-collapse: collapse;
+	}
+	td{
+		border: 1px solid black;
+		min-width: 100px;
+		text-align: center;
+	}
+</style>
+
+
+<h4>Список тестов</h4>
+
+<?php
+$dir = 'tests/';
+$loaded_files = scandir($dir);
+?>
+
+<table>
+	<tr>
+		<td>Id</td>
+		<td>Name of file</td>
+		<td>Try test</td>
+	</tr>
+	<?php foreach ($loaded_files as $key => $value) {
+		if(($value !== '.') && ($value !== '..')){?>
+	<tr>
+		<td><?php 
+		$s_value = substr($value, 0, 3);
+		$i_value = (int)$s_value;
+		print $i_value;
+		 ?></td>
+		<td><?php print $value ?></td>
+		<td>
+			<form method="get" action="test.php">
+				<input type="submit" name="id" value=<?php print '"' . $i_value . '"'?>>
+			</form>
+		</td>
+	</tr>
+<?php } } ?>
+</table>
+
+</body>
+</html>
