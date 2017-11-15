5d5b2bbe254f857ee67973205fbdc1486adee85b
belram <30906145+belram@users.noreply.github.com>
2017-11-15 23:56:32 +0300

    Update 2_1.js

--- javascript/2_1.js
+++ javascript/2_1.js
@@ -73,22 +73,21 @@ const producer3 = {
 var date3 = new Date(2015, 11, 29);
 
 function deferPay(producer, amount, date) {
-	var paymentDate = date.setDate(date.getDate() + producer['deferPeriod']);
-	paymentDate = new Date(paymentDate);
+  var paymentDate = date;
+  paymentDate.setDate(paymentDate.getDate() + producer['deferPeriod']);
 
- 	deferedPayments.push({'producer': producer['name'],
- 	'amount': amount,
- 	'paymentDate': paymentDate});
+	deferedPayments.push({'producer': {'name': producer['name']},
+	'amount': amount,
+	'paymentDate': paymentDate});
 }
 
 deferPay(producer1, 7400, date1);
 deferPay(producer2, 15800, date2);
 deferPay(producer3, 190000, date3);
 
-console.log(`${deferedPayments[0]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[0]['producer']}, сумма ${deferedPayments[0]['amount']} Q`);
-console.log(`${deferedPayments[1]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[1]['producer']}, сумма ${deferedPayments[1]['amount']} Q`);
-console.log(`${deferedPayments[2]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[2]['producer']}, сумма ${deferedPayments[2]['amount']} Q`);
-
+console.log(`${deferedPayments[0]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[0]['producer']['name']}, сумма ${deferedPayments[0]['amount']} Q`);
+console.log(`${deferedPayments[1]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[1]['producer']['name']}, сумма ${deferedPayments[1]['amount']} Q`);
+console.log(`${deferedPayments[2]['paymentDate'].toLocaleDateString('ru-Ru')}: ${deferedPayments[2]['producer']['name']}, сумма ${deferedPayments[2]['amount']} Q`);
 
 //Part 3
 
@@ -96,20 +95,22 @@ function loadCurrencyJSON() {
   return '{"AUD":44.95,"AZN":33.73,"GBP":73.42,"AMD":0.12,"BYN":30.96,"BGN":32.01,"BRL":18.8,"HUF":0.2,"DKK":8.42,"USD":58.85,"EUR":62.68,"INR":0.88,"KZT":0.18,"CAD":44.74,"KGS":0.85,"CNY":8.55,"MDL":2.94,"NOK":7.02,"PLN":14.55,"RON":13.92,"ZZZ":79.91,"SGD":41.36,"TJS":7.43,"TRY":15.97,"TMT":16.84,"UZS":0.02,"UAH":2.16,"CZK":2.32,"SEK":6.6,"CHF":58.69,"ZAR":4.4,"KRW":0.05,"JPY":0.52}';
 }
 
-if(loadCurrencyJSON()){
-	try{
-		var currencyExchange = JSON.parse(loadCurrencyJSON());
-	}catch(err){
-		console.error(err.name, err.message);
-	}
-}
+var currencyExchange;
 
 function convertCurrency(amount, from, to) {
+  currencyExchange = JSON.parse(loadCurrencyJSON());
 	var result = amount * (currencyExchange[from] / currencyExchange[to]);
 	return Math.round(result * 100) / 100;
 }
 
-let price1 = convertCurrency(7000, 'ZZZ', 'USD');
-let price2 = convertCurrency(790, 'EUR', 'ZZZ');
+try{
+  if(loadCurrencyJSON()){
+    var price1 = convertCurrency(7000, 'ZZZ', 'USD');
+    var price2 = convertCurrency(790, 'EUR', 'ZZZ');
+  }
+}catch(err){
+	console.error(err.name, err.message);
+}
+
 console.log(`Сумма ${price1} USD`);
 console.log(`Сумма ${price2} Q`);
