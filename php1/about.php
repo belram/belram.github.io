f631b5ffc03031bdfa8eff87407f469090442ea5
belram <30906145+belram@users.noreply.github.com>
2017-12-25 21:07:50 +0300

    Update and rename 1.php to about.php

---
+++ php1/about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Введение в PHP</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php 
+	$title = 'Страница пользователя Николай';
+	$name = 'Имя';
+	$a = 'Николай';
+	$age = 'Возраст';
+	$b = 30;
+	$mail = 'Адрес электронной почты';
+	$c = 'gor20@tut.by';
+	$city = 'Город';
+	$d = 'Минск';
+	$about_your_self = 'О себе';
+	$i = 'Ищу работу';
+	 ?>
+	
+	 <style>
+	 h1 {
+	 	padding-bottom: 10px;
+	 }
+	 td {
+	 	padding-bottom: 5px;
+	 	padding-right: 10px;
+	 }
+	 </style>
+
+
+
+
+	<h1>
+	<?php 
+	echo $title;
+	 ?>
+	</h1>
+	<table>
+		<tr>
+			<td>
+				<?php 
+				echo $name;
+				 ?>
+			</td>
+			<td>
+				<?php 
+				echo $a;
+				 ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php 
+				echo $age;
+				 ?>
+			</td>
+			<td>
+				<?php 
+				echo $b;
+				 ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php 
+				echo $mail;
+				 ?>
+			</td>
+			<td>
+				<a href="mailto:gor20@tut.by">
+					<?php 
+					echo $c;
+					 ?>
+				</a>	 
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php 
+				echo $city;
+				 ?>
+			</td>
+			<td>
+				<?php 
+				echo $d;
+				 ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php 
+				echo $about_your_self;
+				 ?>
+			</td>
+			<td>
+				<?php 
+				echo $i;
+				 ?>
+			</td>
+		</tr>
+
+	</table>
+</body>
+</html>
