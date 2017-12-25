c3958fd9b8d098e16d5ecc49aee66afda814037b
belram <30906145+belram@users.noreply.github.com>
2017-12-25 21:13:55 +0300

    Update about.php

--- php2/about.php
+++ php2/about.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Простейший PHP сценарий</title>
+	<meta charset="utf-8">
+</head>
+<body>
 
+<p>
+<?php 
+$user = rand(0,100);
+echo "Число ".$user." <br>";
+$one = 1;
+$two = 1;
+while(true){
+	if($one > $user){
+	echo "задуманное число Не входит в числовой ряд ";
+	break 1;
+}elseif($one == $user){
+	echo "задуманное число входит в числовой ряд ";
+	break 2;
+}
+	$three = $one;
+	$one += $two;
+	$two = $three;
+	echo "Three: $three\n";
+	echo "<br>";
+	echo "Two: $two\n";
+}
+ ?>
+</p>
+
+</body>
+</html>
