0872c5f5c097a87dfe508dc4ac378c936f44af79
belram <30906145+belram@users.noreply.github.com>
2018-01-19 20:43:28 +0300

    Create index.php

---
+++ 2.4/index.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'src/session.php';
+
+if (isPost()) {
+    if(login(getParam('login'), getParam('password'))){
+        redirect('list');
+    }elseif(guestMan(getParam('guest'))){
+    	redirect('list');
+    }
+    else {
+        print 'Неверные логин или пароль. Либо введите имя как гость!';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Авторизация</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<form method="POST" action="">
+		<label>Логин:</label><br>
+		<input type="text" name="login" placeholder="login"><br>
+		<label>Пароль:</label><br>
+		<input type="text" name="password" placeholder="password"><br>
+		<input type="submit" name="Вход">
+	</form>
+
+	<form method="POST" action="">
+		<label>Гость:</label><br>
+		<input type="text" name="guest" placeholder="Имя"><br>
+		<input type="submit" name="Пройти тест">
+	</form>
+
+</body>
+</html>
