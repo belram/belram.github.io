c66188d4530ffddc120401bb64d073d147d15608
belram <30906145+belram@users.noreply.github.com>
2018-01-19 20:46:37 +0300

    Create GDgenerate.php

---
+++ 2.4/GDgenerate.php
@@ -0,0 +1,16 @@
+<?php
+function make_img($name, $res){
+	$image = imagecreatetruecolor(1299, 917);
+	$backColor = imagecolorallocate($image, 90, 90, 90);
+	$textColor = imagecolorallocate($image, 120, 120, 120);
+	$fontFile = __DIR__ . '/FONT.ttf';
+	$imBox = imagecreatefrompng(__DIR__ . '/Certif.png');
+	imagefill($image, 0, 0, $backColor);
+	imagecopy($image, $imBox, 0, 0, 0, 0, 1299, 917);
+	imagettftext($image, 45, 0, 620, 500, $textColor, $fontFile, $name);
+	imagettftext($image, 45, 0, 640, 640, $textColor, $fontFile, $res);
+	header('Content-Type: image/png');
+	imagepng($image);
+	imagedestroy($image);
+	imagedestroy($imBox);
+}
