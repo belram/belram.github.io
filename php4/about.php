1d389e2c88eaab1ebd9fcba01929f82f2bf94424
belram <30906145+belram@users.noreply.github.com>
2018-01-23 21:03:14 +0300

    Update about.php

--- php4/about.php
+++ php4/about.php
@@ -1,8 +1,9 @@
 <?php 
 
 $next_data = [];
-function get_weather($filename){
-	$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&units=metric&APPID=a13020b8efb43f553275a23adb4440cb');
+function get_weather($filename, $api, $requestBy, $city, $apid, $if_metric_or_imperial = '', $formatOfDate = ''){
+	$requestData = $api . $requestBy . '=' . $city . '&' . $if_metric_or_imperial . $formatOfDate . '&APPID=' . $apid;
+	$content = file_get_contents($requestData);
 	file_put_contents($filename, $content);
 	$data = json_decode($content, true);
 	global $next_data;
@@ -18,27 +19,66 @@ function have_weather($filename){
 
 function show_weather(){
 	global $next_data;
-	print '<!DOCTYPE html><html><head><title>Погода в Москве</title><meta charset="utf-8"></head><body><style>td{height: 20px;width: 100px;border: 1px solid black;text-align: center;}</style><h1>Погода в Москве</h1><table><tr><td>Текущая температура</td><td>Скорость ветра</td><td>Влажность</td><td>Давление</td></tr><tr>';
-	print "<td>{$next_data['main']['temp']}</td>";
-	print "<td>{$next_data['wind']['speed']}</td>";
-	print "<td>{$next_data['main']['humidity']}</td>";
-	print "<td>{$next_data['main']['pressure']}</td>";
-	print '</tr></table></body></html>';
+
+	print <<<HTML
+	<!DOCTYPE html>
+	<html>
+		<head>
+		<title>Погода в Москве</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<style>
+		td{
+			height: 20px;
+			width: 100px;
+			border: 1px solid black;
+			text-align: center;
+		}
+	</style>
+		<h1>Погода в Москве</h1>
+		<table>
+			<tr>
+				<td>Текущая температура</td>
+				<td>Скорость ветра</td>
+				<td>Влажность</td>
+				<td>Давление</td>
+			</tr>
+			<tr>
+				<td>{$next_data['main']['temp']}</td>
+				<td>{$next_data['wind']['speed']}</td>
+				<td>{$next_data['main']['humidity']}</td>
+				<td>{$next_data['main']['pressure']}</td>
+			</tr>
+		</table>
+	</body>
+	</html>
+HTML;
+
 }
+
+//Параметры запроса
+$api = 'http://api.openweathermap.org/data/2.5/weather?';
+$requestBy = 'q';
+$city = 'Moscow,RU';
+$if_metric_or_imperial = 'units=';
+$formatOfDate = 'metric';
+$apid = 'a13020b8efb43f553275a23adb4440cb';
+///////////////////
 
 $filename = 'cache.txt';
 $time = time() - 3600;
 
 if (file_exists($filename)){
 	if(!touch($filename, $time)){
-		get_weather($filename);
+		get_weather($filename, $api, $requestBy, $city, $apid, $if_metric_or_imperial, $formatOfDate);
 		show_weather();
 	}else{
 		have_weather($filename);
 		show_weather();
 	}
 }else{
-	get_weather($filename);
+	get_weather($filename, $api, $requestBy, $city, $apid, $if_metric_or_imperial, $formatOfDate);
 	show_weather();
 }
 
