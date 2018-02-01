2fc1405372ee4e6cfa5164af1352aed07ab45e10
belram <30906145+belram@users.noreply.github.com>
2018-02-01 20:24:23 +0300

    Update about.php

--- php3.1/about.php
+++ php3.1/about.php
@@ -1,5 +1,4 @@
 <?php 
-
 //First. 
 class Car
 {
@@ -7,7 +6,6 @@ class Car
 	private $tipe;
 	private $price;
 	private $color;
-
 	private $engine;
 	private $sizeOfWheel;
 	private $quantityOfDoors;
@@ -25,28 +23,23 @@ class Car
 
 	public function getDescription()
 	{
-		return ['title' => $this->title, 'tipe' => $this->tipe, 'price' =>  $this->price, 'color' =>  $this->color, 'engine' =>  $this->engine, 'sizeOfWheel' =>  $this->sizeOfWheel, 'quantityOfDoors' =>  $this->quantityOfDoors,];
+		return ['title' => $this->title, 'tipe' => $this->tipe, 'price' =>  $this->price, 'color' =>  $this->color, 'engine' =>  $this->engine, 'sizeOfWheel' =>  $this->sizeOfWheel, 'quantityOfDoors' =>  $this->quantityOfDoors];
 	}
-
 }
 
 $bmw_3 = new Car('BMW 3', 'sedan', 25000, 'red', 2999, 17, 4);
 $renaultLogan = new Car('Renault Logan', 'sedan', 8500, 'white', 1300, 15, 4);
-
 print 'BMW 3 <ul>';
 foreach ($bmw_3->getDescription() as $key => $value) {
 	print "<li>{$key}: $value</li>";
 }
 print '</ul>';
-
 print 'Renault Logan <ul>';
 foreach ($renaultLogan->getDescription() as $key => $value) {
 	print "<li>{$key}: $value</li>";
 }
 print '</ul>';
-
 print '<hr>';
-
 
 //Second.
 class TV
@@ -54,7 +47,6 @@ class TV
 	private $title;
 	private $tipe;
 	private $price;
-
 	private $sizeOfScreen;
 	private $discount;
 
@@ -107,30 +99,24 @@ class TV
 		return $this->sizeOfScreen;
 	}
 
-
 	public function priceWithDiscount()
 	{
 		return round($this->price - ($this->price * $this->discount) / 100);
 	}
-
 }
 
 $assortmentTV = ['LG' => ['tipe' => '3D', 'price' => 400, 'sizeOfScreen' => 40, 'discount' => 5],
 				'SONY' => ['tipe' => '3D', 'price' => 3600, 'sizeOfScreen' => 50, 'discount' => 10],
-				'PASONIC' => ['tipe' => '3D', 'price' => 5000, 'sizeOfScreen' => 60, 'discount' => 7]];
-
+				'PASONIC' => ['tipe' => '3D', 'price' => 5000, 'sizeOfScreen' => 60, 'discount' => 7]
+			];
 $allTV = [];
-
 foreach ($assortmentTV as $key => $value) {
 	$allTV[] = new TV($key, $value['tipe'], $value['price'], $value['sizeOfScreen'], $value['discount']);
 }
-
 foreach ($allTV as $key => $value) {
 	print $value->getTitle() . ' cost ' . $value->priceWithDiscount() . "<br>\n";
 }
-
 print '<hr>';
-
 
 //Third. 
 class BallPen
@@ -138,7 +124,6 @@ class BallPen
 	private $title;
 	private $tipe;
 	private $price;
-
 	private $color;
 	private $num_of_use;
 	private $isInBox;
@@ -156,31 +141,26 @@ class BallPen
 
 	public function canBuyWholesale()
 	{
-		if($this->isInBox == 'Yes'){
+		if ($this->isInBox == 'Yes') {
 			print "You can buy {$this->title} wholesale, if your order more then " . self::SIZE_OF_LOT . " units.<br>\n";
-		}else{
+		} else {
 			print "You can't buy {$this->title} wholesale.<br>\n";
 		}
 	}
 
 }
 
-
 $assortmentPen = ['Hewlett-Packard' => ['tipe' => 'ballpen', 'price' => .6, 'color' => 'black', 'num_of_use' => 'one-off', 'isInBox' => 'No'],
-				'Parker' => ['tipe' => 'ink pen', 'price' => 1, 'color' => 'black', 'num_of_use' => 'reusable', 'isInBox' => 'Yes'],
-				'Senator' => ['tipe' => 'capillary', 'price' => .5, 'color' => 'blue', 'num_of_use' => 'one-off', 'isInBox' => 'No']];
-
+			'Parker' => ['tipe' => 'ink pen', 'price' => 1, 'color' => 'black', 'num_of_use' => 'reusable', 'isInBox' => 'Yes'],
+			'Senator' => ['tipe' => 'capillary', 'price' => .5, 'color' => 'blue', 'num_of_use' => 'one-off', 'isInBox' => 'No']
+			];
 $allPens = [];
-
 foreach ($assortmentPen as $key => $value) {
 	$allPens[] = new BallPen($key, $value['tipe'], $value['price'], $value['color'], $value['num_of_use'], $value['isInBox']);
 }
-
-
 foreach ($allPens as $key => $value) {
 	$value->canBuyWholesale();
 }
-
 print '<hr>';
 
 //Fourth.
@@ -189,7 +169,6 @@ class Duck
 	private $title;
 	private $tipe;
 	private $price;
-
 	private $standardWeight;
 	private $isGeneticallyModified;
 	const IF_GEN_MOD = .8;
@@ -199,11 +178,9 @@ class Duck
 		$this->title = $title;
 		$this->tipe = $tipe;
 		$this->price = $price;
-
 		$this->standardWeight = $standardWeight;
 		$this->isGeneticallyModified = $isGeneticallyModified;
 	}
-
 
 	public function getPriceOrder()
 	{
@@ -212,33 +189,26 @@ class Duck
 
 	private function getPrice()
 	{
-		if($this->isGeneticallyModified == 'Yes'){
+		if ($this->isGeneticallyModified == 'Yes') {
 			return round($this->price * self::IF_GEN_MOD);
-		}else{
+		} else {
 			return $this->price;
 		}
 	}
-
 }
 
 $assortmentDuck = ['Peking Duck' => ['tipe' => 'fillet', 'price' => 7, 'standardWeight' => 2, 'isGeneticallyModified' => 'No'],
-				'Black Duck' => ['tipe' => 'fillet', 'price' => 5, 'standardWeight' => 2, 'isGeneticallyModified' => 'Yes'],
-				'White Duck' => ['tipe' => 'carcass', 'price' => 9, 'standardWeight' => 2, 'isGeneticallyModified' => 'Yes']];
-
-
+			'Black Duck' => ['tipe' => 'fillet', 'price' => 5, 'standardWeight' => 2, 'isGeneticallyModified' => 'Yes'],
+			'White Duck' => ['tipe' => 'carcass', 'price' => 9, 'standardWeight' => 2, 'isGeneticallyModified' => 'Yes']
+		  ];
 $allDuck = [];
-
 foreach ($assortmentDuck as $key => $value) {
 	$allDuck[] = new Duck($key, $value['tipe'], $value['price'], $value['standardWeight'], $value['isGeneticallyModified']);
 }
-
-
 foreach ($allDuck as $key => $value) {
 	$value->getPriceOrder();
 }
-
 print '<hr>';
-
 
 //Fifth. 
 class Product
@@ -270,30 +240,27 @@ class Product
 }
 
 $assortmentProduct = ['iPhone' => ['tipe' => 'mobile phone', 'price' => 1000, 'color' => 'gold', 'size' => '5.5 inch', 'onStock' => 200, 'canBePosted' => 'Yes'],
-				'Boeing' => ['tipe' => 'aircraft', 'price' => 300000000, 'color' => 'silver', 'size' => '210 feets', 'onStock' => 1, 'canBePosted' => 'No'],
-				'Locomotive' => ['tipe' => 'train', 'price' => 2000000, 'color' => 'black', 'size' => '60 feets', 'onStock' => 3, 'canBePosted' => 'No']];
+			'Boeing' => ['tipe' => 'aircraft', 'price' => 300000000, 'color' => 'silver', 'size' => '210 feets', 'onStock' => 1, 'canBePosted' => 'No'],
+			'Locomotive' => ['tipe' => 'train', 'price' => 2000000, 'color' => 'black', 'size' => '60 feets', 'onStock' => 3, 'canBePosted' => 'No']
+		     ];
 
 $allProduct = [];
-
 foreach ($assortmentProduct as $key => $value) {
 	$allProduct[] = new Product($key, $value['tipe'], $value['price'], $value['color'], $value['size'], $value['onStock'], $value['canBePosted']);
 }
-
 $stock =[];
 foreach ($allProduct as $key => $value) {
 	$stock[] = $value->getDescription();
 }
-
-
 print '<table>';
 print '<tr><td>Product</td><td>title</td><td>tipe</td><td>price</td><td>color</td><td>size</td><td>onStock</td><td>canBePosted</td></tr>';
 foreach ($stock as $value) {
 	print "<tr><td>{$value['title']}</td>
-	<td>{$value['tipe']}</td>
-	<td>{$value['price']}</td>
-	<td>{$value['color']}</td>
-	<td>{$value['size']}</td>
-	<td>{$value['onStock']}</td>
-	<td>{$value['canBePosted']}</td></tr>";
+		<td>{$value['tipe']}</td>
+		<td>{$value['price']}</td>
+		<td>{$value['color']}</td>
+		<td>{$value['size']}</td>
+		<td>{$value['onStock']}</td>
+		<td>{$value['canBePosted']}</td></tr>";
 }
 print '</table>';
