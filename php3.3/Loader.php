a888e8e3bddb0a2d4605afe9f9184a18a4be81b6
belram <30906145+belram@users.noreply.github.com>
2018-02-05 20:23:35 +0300

    Update Loader.php

--- php3.3/Loader.php
+++ php3.3/Loader.php
@@ -1,9 +1,10 @@
-<?php 
-    function myAutoload($className) {
-	    $pathToFile = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-	    if (file_exists($pathToFile)) {
-		    include "$pathToFile";
-	    }
+<?php
+    function myAutoload($className) 
+    {
+        $pathToFile = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+        if (file_exists($pathToFile)) {
+            include "$pathToFile";
+        }
     }
 
     spl_autoload_register('myAutoload');
