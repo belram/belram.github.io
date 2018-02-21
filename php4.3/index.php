5b0e1873946f6aa45eb16beb41b3884ce121fb6f
belram <30906145+belram@users.noreply.github.com>
2018-02-21 20:38:02 +0300

    Create index.php

---
+++ php4.3/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Ссылка для авторизации</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<a href="register.php">Войдите на сайт</a>
+</body>
+</html>
