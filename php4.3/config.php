3320bba910c49c5b5dacf08e1aa021fec8eb88ee
belram <30906145+belram@users.noreply.github.com>
2018-02-21 20:38:26 +0300

    Delete config.php

--- php4.3/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=global', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    print "Couldn't insert a row: " . $e->getMessage();
-}
