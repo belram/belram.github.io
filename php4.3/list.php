460da2cbb011dcbec003dc7d1d4c9a85e4f1c254
belram <30906145+belram@users.noreply.github.com>
2018-02-25 12:57:02 +0300

    Update list.php

--- php4.3/list.php
+++ php4.3/list.php
@@ -20,25 +20,20 @@
 			$objK->deliteTask();
 		}
 	}
-
-	if ($_SERVER[REQUEST_METHOD] == 'POST' && isset($_POST['save'])) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save'])) {
 		$obj = new AddTask($db, $_POST['description'], $_SESSION['user']);
 		$obj->addNewTask();
 	}
-
-	if ($_SERVER[REQUEST_METHOD] == 'POST' && isset($_POST['change_executor'])) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['change_executor'])) {
 		$objM = new ChangeExecutor($db, $_POST['new_responsible']);
 		$row = $objM->setNewExecutor();
 	}
-
 	$obj = new ShowAllInformation($_SESSION['user'], $db);
 	$row = $obj->getMyInfo();
-
-	if ($_SERVER[REQUEST_METHOD] == 'POST' && isset($_POST['sort'])) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sort'])) {
 		$objNext = new SortBy($db, $_POST['sort_by'], $_SESSION['user']);
 		$row = $objNext->sortingBy();
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -52,44 +47,38 @@
 		border-collapse: collapse;
 		margin-top: 20px;
 	}
-
 	.col tr:first-child {
 		background-color: #eee;
 		font-weight: bold;
 	}
-
 	td {
 		border: 1px solid black;
 	}
-
 	.col_1 {
 		padding: 0px 10px 0px 10px;
 	}
-
 	.col_11 {
 		padding: 0px 10px 0px 10px;
 		color: orange;
 	}
-
 	.col_111 {
 		padding: 0px 10px 0px 10px;
 		color: green;
 	}
-
 	.col_2 {
 		padding: 0px 5px 0px 10px;
 		text-decoration-line: none;
 	}
-
 	.col_2:hover {
 		text-decoration: underline;
 	}
 </style>
 <body>
-
 	<h2>Добро пожаловать, 
-	<?php	print htmlspecialchars(getAuthorizedUser()['login']); ?>
-	 !!!<h2>
+	<?php	
+	print htmlspecialchars($_SESSION['user']['login']); 
+	?>
+	!!!<h2>
 
 	<h3>Список дел на сегодня</h3>
 		<div style="float: left">
@@ -121,40 +110,42 @@
 			<td>Закрепить задачу за пользователем</td>
 		</tr>
 		<?php
-			foreach ($row as $value) {
-				print '<tr>';
-				print "<td class='col_1'>{$value['description']}</td>\n";
-				print "<td class='col_1'>{$value['date_added']}</td>\n";
-				print "<td class='col_";
-					if ($value['is_done'] == 0) {
-						print "11'>В процессе</td>\n";
-					} else {
-						print "111'>Выполнено</td>\n";
-					}
-				print "<td>\n";
-					print '<a class=\'col_2\' href="' . '?id=' . $value['id'] . '&action=done">Выполнить</a>' . "\n";
-					print '<a class=\'col_2\' href="' . '?id=' . $value['id'] . '&action=delete">Удалить</a>' . "\n";
-				print "</td>\n";
-				print "<td>";
-					if ($value['responsible'] == $_SESSION['user']['login']) {
-						print 'Вы';
-					} else {
-						print "{$value['responsible']}";
-					}
-				print "</td>\n";
-				print "<td>{$_SESSION['user']['login']}</td>";
-				print "<td>\n";
-					print '<form method="POST">';
-		    		print '<select style="margin: 3px;" name="new_responsible">';
-					    $users = $obj->shiftResponsibility();
-		        		foreach ($users as $valueUser) {
-		        			print '<option value="' . $valueUser['id'] . ' ' . $value['id'] . '">' . $valueUser['login'] . '</option>' . "\n";
-		        		}
-		    		print '</select>';
-		    		print '<input style="margin: 3px;" type="submit" name="change_executor" value="Переложить ответственность" />';
-					print '</form>';
-				print "</td>\n";
-				print "</tr>\n";
+			if ($row) {
+				foreach ($row as $value) {
+					print '<tr>';
+					print "<td class='col_1'>" . htmlspecialchars($value['description']) . "</td>\n";
+					print "<td class='col_1'>" . htmlspecialchars($value['date_added']) . "</td>\n";
+					print "<td class='col_";
+						if ($value['is_done'] == 0) {
+							print "11'>В процессе</td>\n";
+						} else {
+							print "111'>Выполнено</td>\n";
+						}
+					print "<td>\n";
+						print '<a class=\'col_2\' href="' . '?id=' . htmlspecialchars($value['id']) . '&action=done">Выполнить</a>' . "\n";
+						print '<a class=\'col_2\' href="' . '?id=' . htmlspecialchars($value['id']) . '&action=delete">Удалить</a>' . "\n";
+					print "</td>\n";
+					print "<td>";
+						if ($value['responsible'] == $_SESSION['user']['login']) {
+							print 'Вы';
+						} else {
+							print htmlspecialchars($value['responsible']);
+						}
+					print "</td>\n";
+					print "<td>" . htmlspecialchars($_SESSION['user']['login']) . "</td>";
+					print "<td>\n";
+						print '<form method="POST">';
+			    		print '<select style="margin: 3px;" name="new_responsible">';
+						    $users = $obj->shiftResponsibility();
+			        		foreach ($users as $valueUser) {
+			        			print '<option value="' . htmlspecialchars($valueUser['id']) . ' ' . htmlspecialchars($value['id']) . '">' . htmlspecialchars($valueUser['login']) . '</option>' . "\n";
+			        		}
+			    		print '</select>';
+			    		print '<input style="margin: 3px;" type="submit" name="change_executor" value="Переложить ответственность" />';
+						print '</form>';
+					print "</td>\n";
+					print "</tr>\n";
+				}
 			}
 		?>
 	</table>
@@ -173,24 +164,26 @@
 		<?php
 		$obj = new ShowAllInformation($_SESSION['user'], $db);
 		$oterTask = $obj->getOtherInfo();
-		foreach ($oterTask as $value) {
-			print '<tr>';
-			print "<td class='col_1'>{$value['description']}</td>\n";
-			print "<td class='col_1'>{$value['date_added']}</td>\n";
-			print "<td class='col_";
-				if ($value['is_done'] == 0) {
-					print "11'>В процессе</td>\n";
-				} else {
-					print "111'>Выполнено</td>\n";
-				}
-			print "<td>\n";
-				print '<a class=\'col_2\' href="' . '?id=' . $value['id'] . '&action=done1">Выполнить</a>' . "\n";
-				print '<a class=\'col_2\' href="' . '?id=' . $value['id'] . '&action=delete1">Удалить</a>' . "\n";
-			print "</td>\n";
-			print "<td>{$_SESSION['user']['login']}</td>\n";
-			print "<td>{$value['author']}</td>\n";
-			print "</tr>\n";
-		}
+		if($oterTask) {
+			foreach ($oterTask as $value) {
+				print '<tr>';
+				print "<td class='col_1'>" . htmlspecialchars($value['description']) . "</td>\n";
+				print "<td class='col_1'>" . htmlspecialchars($value['date_added']) . "</td>\n";
+				print "<td class='col_";
+					if ($value['is_done'] == 0) {
+						print "11'>В процессе</td>\n";
+					} else {
+						print "111'>Выполнено</td>\n";
+					}
+				print "<td>\n";
+					print '<a class=\'col_2\' href="' . '?id=' . htmlspecialchars($value['id']) . '&action=done1">Выполнить</a>' . "\n";
+					print '<a class=\'col_2\' href="' . '?id=' . htmlspecialchars($value['id']) . '&action=delete1">Удалить</a>' . "\n";
+				print "</td>\n";
+				print "<td>" . htmlspecialchars($_SESSION['user']['login']) . "</td>\n";
+				print "<td>" . htmlspecialchars($value['author']) . "</td>\n";
+				print "</tr>\n";
+			}
+		}	
 		?>
 	</table>
 	<?php
