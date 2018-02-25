087f975c6241cc94b371d2556c89ea21da6732d4
belram <30906145+belram@users.noreply.github.com>
2018-02-25 12:58:05 +0300

    Update register.php

--- php4.3/register.php
+++ php4.3/register.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/core/session.php';
+require_once(__DIR__ . '/core/session.php');
 
 if (isPost()) {
 	if (isset($_POST['sign_in'])) {
@@ -11,15 +11,20 @@ if (isPost()) {
 	    }
 	} elseif (isset($_POST['register'])) {
 		if (login(getParam('login'), getParam('password'), $db)) {
-	        print '</label>Такой пользователь уже существует в базе данных.</label>';
+	        print '<label>Такой пользователь уже существует в базе данных.</label>';
 	        print '<hr>';
 	    }elseif (!login(getParam('login'), getParam('password'), $db)) {
-	    	$obj = new Registration(getParam('login'), getParam('password'), $db);
-	    	if($obj->createUser()){
-	    		redirect('list');
-	    	}else{
-	    		print '</label>Логин и/или пароль менее 1 символа не допустимы!</label>';
-	    	}
+	    	if (!isUser(getParam('login'), $db)){
+		    	$obj = new Registration(getParam('login'), getParam('password'), $db);
+		    	if($obj->createUser()){
+		    		redirect('list');
+		    	}else{
+		    		print '<label>Логин и/или пароль менее 1 символа не допустимы!</label>';
+		    	}
+		    }else{
+		    	print '<label>Выберете другой логин!</label>';
+		    	print '<hr>';
+		    }	
 	    }
 	}
 }
@@ -31,18 +36,18 @@ if (isPost()) {
 	<title>Авторизация и регистрация</title>
 	<meta charset="utf-8">
 </head>
-<body>
+	<body>
 	<?php
-	if (!isPost()) {
+	if (!isPost() && (!isset($_POST['sign_in']) || !isset($_POST['register']))) {
 		print '<label>Введите данные для регистрации или войдите, если уже регистрировались:</label>';
 		print '<hr>';
 	}
 	?>
-	<form method="POST" action="">
-		<input type="text" name="login" placeholder="login">
-		<input type="text" name="password" placeholder="password">
-		<input type="submit" name="sign_in" value="Вход">
-		<input type="submit" name="register" value="Регистрация">
-	</form>
-</body>
+		<form method="POST" action="">
+			<input type="text" name="login" placeholder="login">
+			<input type="text" name="password" placeholder="password">
+			<input type="submit" name="sign_in" value="Вход">
+			<input type="submit" name="register" value="Регистрация">
+		</form>
+	</body>
 </html>
