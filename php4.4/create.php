2d391f71712440243baf9a1eea360ceea9d3804b
belram <30906145+belram@users.noreply.github.com>
2018-03-02 23:44:34 +0300

    Create create.php

---
+++ php4.4/create.php
@@ -0,0 +1,26 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'bulai';
+    $user = '';
+    $password = '';
+
+    try {
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        print "Couldn't link: " . $e->getMessage();
+    }
+
+    $sql = 'CREATE TABLE `students` (
+        `id` int(11) NOT NULL AUTO_INCREMENT, 
+        `name` varchar(50) NOT NULL,
+        `estimation` float NOT NULL,
+        `budjet` tinyint(4) NOT NULL DEFAULT 0,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8';
+
+    try {
+        $q = $db->exec($sql);
+    } catch (PDOException $e) {
+        print "Couldn't create a table: " . $e->getMessage();
+    }
