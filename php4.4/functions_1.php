ac70371b36f5ba4e481faf02cf72d31126caedb7
belram <30906145+belram@users.noreply.github.com>
2018-03-02 23:51:21 +0300

    Create functions_1.php

---
+++ php4.4/functions_1.php
@@ -0,0 +1,8 @@
+require_once __DIR__ . '/../conf/config.php';
+
+	try {
+	    $q = $db->query('SHOW TABLES');
+	    $row = $q->fetchAll(PDO::FETCH_ASSOC);
+	} catch (PDOException $e) {
+	    print "Couldn't get TABLES: " . $e->getMessage();
+	}
