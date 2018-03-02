d0f95eb51013b499633a82f60e1df2447e4f46bb
belram <30906145+belram@users.noreply.github.com>
2018-03-02 23:52:11 +0300

    Create config.php

---
+++ php4.4/conf/config.php
@@ -0,0 +1,10 @@
+$host = 'localhost';
+$dbname = 'bulai';
+$user = '';
+$password = '';
+try {
+    $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $user, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print "Couldn't link: " . $e->getMessage();
+}
